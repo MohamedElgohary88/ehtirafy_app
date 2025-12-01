558a2e2b75fc845f3a7d86711a878f57897208f8
mohamedelgohary <mohamed.abdelazim.elgohary@gmail.com>
2025-12-01 19:36:21 +0200

    init code

--- lib/core/widgets/app_logo.dart
+++ lib/core/widgets/app_logo.dart
@@ -1,19 +1,2 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class AppLogo extends StatelessWidget {
-  final double size; // logical size reference (pre-scale)
-  final String assetPath;
-
-  const AppLogo({super.key, this.size = 120, this.assetPath = 'assets/images/logo.png'});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: size.w,
-      height: size.w,
-      child: Image.asset(assetPath, fit: BoxFit.contain),
-    );
-  }
-}
 
